7be9c6e36c7c5b363649f9e9a16a51215bf66b19
Kholmumin <kholmumintursinboev@gmail.com>
2026-02-10 19:57:02 +0500

    Removed SwiftUI

--- ABCTest/AppDelegate.swift
+++ ABCTest/AppDelegate.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-import SwiftUI
 
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -27,18 +27,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
-}
-
-@main
-struct TaskApp: App {
-    let diContainer = AppSwiftUIDIContainer()
-
-    var body: some Scene {
-        WindowGroup {
-             AppSwiftUIRootView(diContainer: diContainer)
-        }
     }
 }
